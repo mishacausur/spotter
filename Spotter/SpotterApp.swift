5397a9a8ac580d1e293dc01cc07e8d405f059b1b
Misha Causur <mishacausur@mail.ru>
2024-03-31 21:54:15 +0500

    Initial Commit

---
+++ Spotter/SpotterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotterApp.swift
+//  Spotter
+//
+//  Created by Misha Causur on 31.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SpotterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
